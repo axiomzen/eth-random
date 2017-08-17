2ea5ade97b5bd24bc23b27256aa2e32aa1e8ed06
Kai Aldag <kaialdag@Kais-MacBook-Pro.local>
2017-08-17 10:25:25 -0700

    remove comments

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -7,23 +7,3 @@ module.exports = function(deployer) {
     deployer.deploy(RandomTestWrapper);
   }
 };
-
-
-// var Random = artifacts.require("./Random.sol");
-// var RandomTestWrapper = artifacts.require("./RandomTestWrapper.sol")
-//
-// module.exports = function(deployer) {
-//   console.log('a');
-//   deployer.deploy(Random).then(function(random) {
-//     console.log('a');
-//     if (deployer.network == 'development') {
-//       console.log('a');
-//       deployer.deploy(RandomTestWrapper).then(function(test) {
-//         console.log('a');
-//         test.setRandom(random);
-//         deployer.link(Random, RandomTestWrapper)
-//         console.log('a');
-//       });
-//     }
-//   });
-// };
