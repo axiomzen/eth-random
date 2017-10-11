8ba0e5dbebdba4cc079fecf58ffe04a1bd73f6d9
FabianoPS <fabianosoriani@gmail.com>
2017-10-10 17:30:08 -0700

    add maxRandom to api

--- API/RandomAPI.sol
+++ API/RandomAPI.sol
@@ -1,6 +1,7 @@
 pragma solidity ^0.4.4;
 
 contract RandomAPI {
-  function random(uint256 upper) public returns (uint256 randomNumber);
-  function random16() public returns(uint16[16]);
+    function maxRandom() public returns (uint256 randomNumber);
+    function random(uint256 upper) public returns (uint256 randomNumber);
+    function random16() public returns(uint16[16]);
 }
