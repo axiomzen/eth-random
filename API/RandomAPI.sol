bbf746e44bed253c21f7526fe7f5f5eb74dd3cd8
FabianoPS <fabianosoriani@gmail.com>
2017-11-14 15:25:08 -0800

    Improve docs

--- API/RandomAPI.sol
+++ API/RandomAPI.sol
@@ -3,5 +3,4 @@ pragma solidity ^0.4.4;
 contract RandomAPI {
     function maxRandom() public returns (uint256 randomNumber);
     function random(uint256 upper) public returns (uint256 randomNumber);
-    function random16() public returns(uint16[16]);
 }
