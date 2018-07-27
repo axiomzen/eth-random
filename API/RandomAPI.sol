2ea5f7227efe5faaa9083065a4b095811eee3b0a
FabianoPS <fabianosoriani@gmail.com>
2018-07-27 16:30:11 -0700

    update README, remove deprecated code

--- API/RandomAPI.sol
+++
@@ -1,6 +0,0 @@
-pragma solidity ^0.4.4;
-
-contract RandomAPI {
-    function maxRandom() public returns (uint256 randomNumber);
-    function random(uint256 upper) public returns (uint256 randomNumber);
-}
